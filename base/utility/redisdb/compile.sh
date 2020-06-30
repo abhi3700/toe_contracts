2b5943bc34156c23a3cb3315a470f2ddb022d555
Abhijit Roy <dalvath3700@gmail.com>
2020-07-01 02:18:38 +0530

    redisdb removed

--- base/utility/redisdb/compile.sh
+++
@@ -1,1 +0,0 @@
-eosio-cpp -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8 -I/usr/include/c++/8/backward -I/usr/lib/gcc/x86_64-linux-gnu/8/include -I/usr/local/include -I/usr/lib/gcc/x86_64-linux-gnu/8/include-fixed -I/usr/include/x86_64-linux-gnu -I/usr/include -lredis++ -lhiredis redisdb.cpp -o redisdb.wasm
