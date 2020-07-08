b2c1b9d0a902dbb4f64fcee1669b835f695386a8
Abhijit Roy <dalvath3700@gmail.com>
2020-07-09 04:50:13 +0530

    cabeosride hpp, cpp updated

--- cabeosride/include/cabeosride.hpp
+++ cabeosride/include/cabeosride.hpp
@@ -1,30 +1,200 @@
 #include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
 
 using eosio::contract;
 using eosio::print;
-using std::string;
+using eosio::name;
 using eosio::multi_index;
+using eosio::indexed_by;
+using eosio::asset;
+using eosio::check;
+using eosio::permission_level;
+
+using std::string;
 
 class [[eosio::contract]] cabeosride : public contract
 {
+
+private:
+	const symbol ride_token_symbol;
+
 public:
 	using contract::contract;
-	
-	[[eosio::action]]
-	void create(name dummy_ac) {
-		require_auth( get_self() );
 
-		ride_index rides(get_self(), )
+	cabeosride(name receiver, name code, datastream<const char*> ds) : 
+				contract(receiver, code, ds), 
+				ride_token_symbol("TOE", 4) {}
+	
+	/**
+	 * @brief - change source location
+	 * @details 
+	 * 		- search by commuter_ac,
+	 * 		- change source's latitude & longitude
+	 * 
+	 * @param commuter_ac
+	 * @param src_lat 
+	 * @param src_lon
+	 */
+	[[eosio::action]]
+	void create(
+		const name& commuter_ac,
+		double src_lat, 
+		double src_lon, 
+		double des_lat, 
+		double des_lon,
+		string vehicle_type 
+		);
+
+
+	/**
+	 * @brief - cancel ride
+	 * @details - cancel ride (if any created) by commuter
+	 * 
+	 * @param commuter_ac - commmuter account
+	 */
+	[[eosio::action]]
+	void cancelbycom(
+		const name& commuter_ac
+		);
+
+	/**
+	 * @brief - cancel ride
+	 * @details - cancel ride (if any created) by driver
+	 * 
+	 * @param driver_ac - commmuter account
+	 */
+	[[eosio::action]]
+	void cancelbydri(
+		const name& driver_ac
+		);
+
+	/**
+	 * @brief - change source location
+	 * @details 
+	 * 		- search by commuter_ac,
+	 * 		- change source's latitude & longitude
+	 * 
+	 * @param commuter_ac
+	 * @param src_lat 
+	 * @param src_lon
+	 */
+	[[eosio::action]]
+	void changesrc(
+		const name& commuter_ac,
+		double src_lat, 
+		double src_lon 
+		);
+
+	/**
+	 * @brief change destination location
+	 * @details 
+	 * 		- search by commuter_ac,
+	 * 		- change destination's latitude & longitude
+	 * 
+	 * @param commuter_ac - commuter account
+	 * @param des_lat - destination latitude
+	 * @param des_lon - destination longitude
+	 */
+	[[eosio::action]]
+	void changedes(
+		const name& commuter_ac,
+		double des_lat, 
+		double des_lon 
+		);
+
+	[[eosio::action]]
+	void alert() {
 
 	}
+
+	/**
+	 * @brief start ride
+	 * @details
+	 * 		- search table by `commuter_ac` 
+	 * 		- change ride_status (enroute --> ontrip)
+	 * 
+	 * @param driver_ac - driver account
+	 * @param commuter_ac - commuter account
+	 */
+	[[eosio::action]]
+	void start(
+		const name& driver_ac,
+		const name& commuter_ac
+		);
+
+	/**
+	 * @brief - finish ride
+	 * @details
+	 * 		- search table by `driver_ac` 
+	 * 		- change ride_status (ontrip --> completed)
+	 * 
+	 * @param driver_ac - driver account
+	 */
+	[[eosio::action]]
+	void finish(
+		const name& driver_ac,
+		);
+
+
+	/**
+	 * @brief - send fare to the contract
+	 * @details - send the `fare_est` to the contract before ride for automatic deduction.
+	 * 
+	 * @param commuter_ac - commuter account
+	 * @param contract_ac - contract account
+	 * @param quantity - the fare_est amount
+	 * @param memo - remarks
+	 */
+	[[eosio::on_notify("toe1111token::transfer")]]
+	void sendfare(
+		name commuter_ac, 
+		name contract_ac, 
+		asset quantity, 
+		string memo
+		);
+
+
 
 
 private:
 	struct [[eosio::table]] ride
 	{
+		name commuter_ac;
+		name ride_status;		// enroute, ontrip, finished
 		name driver_ac;
-		string ride_status,
+		double src_lat; 
+		double src_lon; 
+		double des_lat; 
+		double des_lon;
+		string pay_mode;			// crypto or fiat (includes cash)
+		string pay_status;			// preride or unpaid or postride
+		string assign_timestamp;	// at which ride is assigned
+		string pickup_point_timestamp;	// at which driver reached source location to pick-up
+		string start_timestamp;		// at which the ride is started
+		string finish_timestamp		// at which the ride is finished
+		double fare_est;			// estimated fare
+		double fare_act;			// actual fare
+
+
+
+		auto primary_key() const { return commuter_ac.value; }
+		auto get_secondary_1() const { return driver_ac.value; }
+		auto get_secondary_2() const { return ride_status.value; }
 	};
 
-	using ride_index = multi_index<"ridetable"_n, ride>
+	using ride_index = multi_index<"rides"_n, ride, 
+									indexed_by<"bydriver"_n, const_mem_fun<ride, uint64_t, &ride::get_secondary_1>>
+									indexed_by<"byridestatus"_n, const_mem_fun<ride, uint64_t, &ride::get_secondary_2>>
+									>;
+
+
+// -----------------------------------------------------------------------------------------------------------------------
+	struct [[eosio::table]] fareamount
+	{
+		asset balance;
+
+		auto primary_key() const { return balance.amount; }
+	};
+
+	using fareamount_index = multi_index<"fare"_n, fareamount>;
 };
--- cabeosride/src/cabeosride.cpp
+++ cabeosride/src/cabeosride.cpp
@@ -1,5 +1,60 @@
-#include <cabeosride.hpp>
-ACTION cabeosride::hi( name nm ) {
-   /* fill in action body */
-   print_f("Name : %\n",nm);
+void cabeosride::sendfare(
+	name commuter_ac, 
+	name contract_ac, 
+	asset quantity, 
+	string memo
+	) {
+
+	// check for conditions if either or both of these are true for `from` & `to`
+	if(contract_ac != get_self() ||  commuter_ac == get_self()) {		// atleast one of the condition is true
+		print("Either money is not sent to the contract")
+		return;
+	}
+
+
+	// @TODO - if symbol doesn't match then, return the money back. So, fire an inline action to external contract for transfer back to commuter_ac
+	// Check for token symbol to be 'TOE'
+	// check( quantity.symbol == ride_token_symbol, "The token transferred is different");
+	if(quantity.symbol != ride_token_symbol) {
+		action(
+			permission_level{get_self(), "active"_n},
+			"toe1111token"_n,
+			"transfer",
+			std::make_tuple(get_self(), commuter_ac, quantity, "Return money as the symbol is different.")
+			).send();
+		return;
+	}
+
+	// instantiate the `fareamount` table
+	fareamount_index fareamount_table(get_self(), commuter_ac.value);
+	auto fare_it = fareamount_table.find(ride_token_symbol.raw());
+
+	// update (add/modify) the balance
+	if(fare_it == fareamount_table.end()) {
+		fareamount_table.emplace(get_self(), [&](auto& row) {
+			row.balance = quantity;
+		});
+	} else {
+		fareamount_table.modify(fare_it, get_self(), [&](auto& row) {
+			row.balance += quantity;
+		});
+	}
+
+	// instantiate the `ride` table
+	ride_index ride_table(get_self(), get_self().value);
+	auto ride_it = ride_table.find(commuter_ac.value);
+
+	// update(add/modify) the `pay_mode` & `pay_status`
+	if(ride_it == ride_table.end()) {
+		ride_table.emplace(commuter_ac, [&] (auto& row) {
+			row.pay_mode = "crypto";
+			row.pay_status = "preride";
+		});
+	} else {
+		ride_table.modify(it, commuter_ac, [&] (auto& row) {
+			row.pay_mode = "crypto";
+			row.pay_status = "preride";
+		});
+	}
+
 }
