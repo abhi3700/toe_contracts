45c4918a7295f724a2e57bfd21b3f043626e3389
Abhijit Roy <dalvath3700@gmail.com>
2020-08-16 01:59:37 +0530

    toeridewallet: ridestaxi table updated as per the change

--- toeridewallet/include/toeridewallet.hpp
+++ toeridewallet/include/toeridewallet.hpp
@@ -172,21 +172,26 @@ private:
 		name commuter_ac;
 		name ride_status;           // /requested/enroute/waiting/ontrip/complete
 		name driver_ac;
-		checksum256 src_lat_hash; 
-		checksum256 src_lon_hash; 
-		checksum256 des_lat_hash; 
-		checksum256 des_lon_hash;
+		string ride_id;		// a unique id of the ride. Used for rating in `userauth` table in toeuserauth contract
+		string src_lat_hash; 
+		string src_lon_hash; 
+		string des_lat_hash; 
+		string des_lon_hash;
 		name vehicle_type;      // list of taxis - toeauto, toemoto, toego, toegoexec, toepremier, toepremexec, toexl, toegointcity, toexlintcity
 		uint32_t seat_count;        // set for pool, else default is 2
 		name pay_mode;            // crypto or fiatdigi or fiatcash
 		name crypto_paystatus;          // paidbycom or paidtodri for "crypto"
 		name fiat_paystatus;          // paidbycom or paidtodri	for "fiatdigi"
-		uint32_t assign_timestamp;  // at which ride is assigned
+		uint32_t create_timestamp;			// at which ride is created
+		uint32_t assign_timestamp;  		// at which ride is assigned
 		uint32_t reachsrc_timestamp_est;    // at which driver is estimated to reach source location to pick-up
 		uint32_t reachsrc_timestamp_act;    // at which driver actually reached source location to pick-up
+		uint32_t cancel_timestamp;
 		uint32_t start_timestamp;       // at which the ride is started
+		uint32_t changedes_timestamp;		// at which commuter changes destination(s). It can be multiple. So, if > 1, then shown timestamp will be the last changedes.
 		uint32_t finish_timestamp_act;      // at which the ride is finished
 		uint32_t finish_timestamp_est;      // at which the ride is estimated to finish
+		uint32_t addfareact_timestamp;		// at which the fare_act is added
 		name ridex_usagestatus_com;					// "y"_n or "n"_n, only 1 ride is used by default.
 		name ridex_usagestatus_dri;					// "y"_n or "n"_n, only 1 ride is used by default.
 		float fare_est;			// estimated fare (in national curr)
@@ -194,7 +199,10 @@ private:
 		float market_price;		// market price during ride request
 		asset fare_crypto_est;			// estimated fare (in national curr) converted (outside blockchain interaction) to fare (in crypto) based on the market rate.
 		asset fare_crypto_act;			// actual fare (in national curr) converted (outside blockchain interaction) to fare (in crypto) based on the market rate.
-
+		name rating_status_dri;			// rating status of doer - driver | "done"
+		float rating_dri;				// rating of driver set by commuter | < 5.0
+		name rating_status_com;			// rating status of doer - commuter | "done"
+		float rating_com;				// rating of commuter set by driver | < 5.0
 
 		auto primary_key() const { return commuter_ac.value; }
 		uint64_t get_secondary_1() const { return driver_ac.value; }
